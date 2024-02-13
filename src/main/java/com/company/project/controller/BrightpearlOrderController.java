a57f7614ed24d9218313a12b4eaea500105720cf
Cody Teng <codyt@darwynn.com>
2024-02-13 16:09:01 -0500

    modify project

---
+++ src/main/java/com/company/project/controller/BrightpearlOrderController.java
@@ -0,0 +1,26 @@
+package com.company.project.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/brightpearl/queen")
+public class BrightpearlOrderController {
+
+    @PostMapping("/orders")
+    public List<String> getBatchOrder() {
+
+
+
+        List<String> results = new ArrayList<>();
+
+        results.add("order1");
+        results.add("order2");
+
+        return results;
+    }
+}
